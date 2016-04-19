e289481ce7b3dcac96a1a5ae523e9b57faa7ec2a
Hussam A. El-Araby <hussam.a@aucegypt.edu>
2016-04-19 04:02:28 +0200

    done

--- src/tlistd/tlistd.c
+++ src/tlistd/tlistd.c
@@ -8,24 +8,32 @@
 #include <linux/sched.h>
 #include <linux/printk.h>
 
+void dfs(struct task_struct *task)
+{
+	struct task_struct *next_task;
+	struct list_head *list;
+
+	printk(KERN_INFO "PID: %d    State: %ld    Name: %s\n", task->pid, task->state, task->comm);
+
+	list_for_each(list, &task->children) 
+	{
+		next_task = list_entry(list, struct task_struct, sibling);
+		dfs(next_task);
+	}
+}
+
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-	printk(KERN_INFO "Loading tlist linear Module\n");
-
-	struct task_struct *task;
-	for_each_process(task)
-	{
-		printk(KERN_INFO "PID: %d    State: %ld    Name: %s\n", task->pid, task->state, task->comm);
-	}
-
-   return 0;
+	printk(KERN_INFO "Loading tlist DFS Module\n");
+	dfs(&init_task);
+	return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) 
 {
-	printk(KERN_INFO "Removing tlist linear Module\n");
+	printk(KERN_INFO "Removing tlist DFS Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
